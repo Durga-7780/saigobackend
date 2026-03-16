f7cb0373b4eef57c31d06084bcae394ceb57c698
Durga <167679258+Durga-7780@users.noreply.github.com>
2026-03-16 22:29:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -107,10 +107,21 @@ app = FastAPI(
 )
 #allow_origins=["*"]
 # # Configure CORS
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["https://saigo.onrender.com"],
+#     allow_credentials=settings.CORS_ALLOW_CREDENTIALS,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://saigo.onrender.com"],
-    allow_credentials=settings.CORS_ALLOW_CREDENTIALS,
+    allow_origins=[
+        "https://saigo.onrender.com",
+        "http://localhost:3000"
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
